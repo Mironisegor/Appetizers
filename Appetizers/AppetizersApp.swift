4ab6cf313020d821bd6acf1aa00f093c8d9ce954
Mironisegor <mironove55@icloud.com>
2024-04-15 15:55:55 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Vladimir Fursov on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appetizers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by Vladimir Fursov on 15.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
