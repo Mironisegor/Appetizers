e6d1b158eeaf22bf2a8a52663b53ac22aaf358af
Egor Mironov <mironobe55@icloud.com>
2024-04-15 19:02:20 +0300

    Commit from Gleb laptop

---
+++ Appetizers/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  Appetizers
+//
+//  Created by Gleb  on 15.04.2024.
+//
+
+import Foundation
+
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+}
