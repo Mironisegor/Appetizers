6be16fa059c533d56975e90f9d64263e5a2fa0d4
Mironisegor <mironove55@icloud.com>
2024-04-15 16:53:38 +0300

    /

---
+++ Appetizers/AppetizerListView.swift
@@ -0,0 +1,18 @@
+//
+//  AppetizerListView.swift
+//  Appetizers
+//
+//  Created by Vladimir Fursov on 15.04.2024.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerListView()
+}
---
+++ Appetizers/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  Appetizers
+//
+//  Created by Vladimir Fursov on 15.04.2024.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}
